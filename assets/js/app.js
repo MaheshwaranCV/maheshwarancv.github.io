8bbcc1984b4eb390752aa50c7bb46cba08a33b38
Maheshwaran C V <117991342+MaheshwaranCV@users.noreply.github.com>
2024-05-15 10:11:41 +0000

    14

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ document.body.appendChild(script);
 // Header
 
 let header = $(`
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="navbar">
+<nav class="navbar navbar-expand-lg fixed-top dark-theme" id="navbar">
 <a class="navbar-brand" href="index.html">  Mahesh</a>
 <div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
