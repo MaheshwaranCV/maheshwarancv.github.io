2213349163ab1167f69ad36886ab58ee524ccbde
Maheshwaran C V <117991342+MaheshwaranCV@users.noreply.github.com>
2024-05-16 10:12:52 +0000

    33

--- assets/js/sem_temp.js
+++ assets/js/sem_temp.js
@@ -22,7 +22,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/u/1/d/1_JeiCu6MJmpIgnOk3nQbyHigZuBlm2rw/view?usp=sharing" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
    </li>
