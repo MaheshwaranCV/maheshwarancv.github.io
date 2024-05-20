94b3662c40850fcce1059c193c5f89fd57dd9b58
Maheshwaran C V <117991342+MaheshwaranCV@users.noreply.github.com>
2024-05-20 12:13:21 +0000

    86

--- assets/js/project.js
+++ assets/js/project.js
@@ -11,6 +11,7 @@ const projects = [
     description: "A quiz app built using basic react.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Vendor Portal",
@@ -18,6 +19,7 @@ const projects = [
     description: "A chess engine for a popular game dev engine.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Employee Portal",
@@ -25,6 +27,7 @@ const projects = [
     description: "Flappy bird game built using React.js",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Maintenance Portal",
@@ -32,6 +35,7 @@ const projects = [
     description: "Exercise tracker built using basic redux.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Shop Floor Portal",
@@ -39,6 +43,7 @@ const projects = [
     description: "Recipe Ingredient Parser built using JavaScript.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "EHSM Portal",
@@ -46,14 +51,15 @@ const projects = [
     description: "Poker game built using typescript.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Quality Portal",
     cardImage: "assets/images/project-page/QualityPortal.jpg",
-    description:
-      "Tested a React Tic Tac Toe Game using Jest and React Testing Library.",
+    description: "Tested a React Tic Tac Toe Game using Jest and React Testing Library.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   {
     title: "Sales Forecast Application",
@@ -61,6 +67,7 @@ const projects = [
     description: "Personal book library app built using JavaScript.",
     Previewlink: "",
     Githublink: "",
+    Videolink: "",
   },
   // {
   //   title: "Minesweeper Game",
@@ -96,28 +103,44 @@ const projects = [
 // function for rendering project cards data
 const showCards = () => {
   let output = "";
-  projects.forEach(({ title, cardImage, Previewlink, Githublink }) => {
+  projects.forEach(({ title, cardImage, Previewlink, Githublink, Videolink }) => {
     output += `       
-        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
-          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-            <div class="header">
-            </div>
-            <div class="data">
-              <div class="content">
+      <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600">
+        <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+          <div class="header"></div>
+          <div class="data">
+            <div class="content">
               <div class="title-div">
                 <h1 class="title"><a href="#">${title}</a></h1>
-                </div>
-            <ul class="menu-content"><br>
-                  <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
-                  <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
-                </ul>
               </div>
+              <ul class="menu-content"><br>
+                <li><a href="${Previewlink}" class="social-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor">
+                    <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                    <line x1="8" y1="21" x2="16" y2="21"></line>
+                    <line x1="12" y1="17" x2="12" y2="21"></line>
+                  </svg>
+                </a></li>
+                <li><a href="${Githublink}" class="social-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a></li>
+                <li><a href="${Videolink}" class="social-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-video">
+                    <polygon points="23 7 16 12 23 17 23 7"></polygon>
+                    <rect x="1" y="5" width="20" height="14" rx="2" ry="2"></rect>
+                  </svg>
+                </a></li>
+              </ul>
             </div>
           </div>
-        </div>`;
+        </div>
+      </div>`;
   });
   projectcards.innerHTML = output;
 };
+
 document.addEventListener("DOMContentLoaded", showCards);
 
 function myFunction() {
