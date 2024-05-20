b3bf300c1e49209d9f21963c61bdb107fd96f583
Maheshwaran C V <117991342+MaheshwaranCV@users.noreply.github.com>
2024-05-20 15:15:04 +0000

    101

--- assets/js/project.js
+++ assets/js/project.js
@@ -76,7 +76,7 @@ const showCards = () => {
   let output = "";
   projects.forEach(({ title, cardImage, description, Previewlink, Githublink, Videolink }) => {
     output += `       
-        <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
+        <div class="column skill-card card" style="margin: 5px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
             <div class="header">
             </div>
